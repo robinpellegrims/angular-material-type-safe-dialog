18f2d58ba48f6ca5a9e1f482c978cdf2cb1fca99
robinpellegrims <realbirkoff@gmail.com>
2022-06-02 20:17:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-uqqfbm-inuvje
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-uqqfbm-inuvje)
---
+++ src/app/dialog-data-example.ts
@@ -0,0 +1,38 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: 'panda' | 'unicorn' | 'lion';
+}
+
+/**
+ * @title Injecting data when opening a dialog
+ */
+@Component({
+  selector: 'dialog-data-example',
+  templateUrl: 'dialog-data-example.html',
+})
+export class DialogDataExample {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogDataExampleDialog, {
+      data: {
+        animal: 'panda',
+      },
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-data-example-dialog',
+  templateUrl: 'dialog-data-example-dialog.html',
+})
+export class DialogDataExampleDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
